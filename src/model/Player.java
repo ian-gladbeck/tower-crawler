e093f5901c8a442506f2ec43114b8048b5b09571
Ian <iian.dev66@gmail.com>
2026-07-30 01:27:13 -0300

    feat: add class Player

--- src/model/Player.java
+++ src/model/Player.java
@@ -3,4 +3,23 @@ package model;
 public class Player {
     private String name;
     private int life;
+    private Position position;
+
+    public Player(String name, int life, Position position) {
+        this.name = name;
+        this.life = life;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
 }
