cb844230327d107e109919b24821533fe4b3fd5b
Ian <iangladbeck@gmail.com>
2026-08-27 18:01:17 -0400

    refactor: handle market engine exceptions and prevent gold charge on item addition failure

--- src/engine/MarketEngine.java
+++ src/engine/MarketEngine.java
@@ -1,13 +1,13 @@
 package engine;
 
+import exception.AlreadyHasItemException;
+import exception.InsufficientGoldException;
+import exception.InventoryFullException;
 import model.Player;
 import model.items.HealingPotion;
-import model.items.Item;
 import model.items.Sword;
-import model.items.SwordType;
 import repository.ItemRepository;
 import ui.ConsoleUI;
-import java.util.Map;
 
 public class MarketEngine {
     private final ItemRepository itemRepository;
@@ -20,51 +20,50 @@ public class MarketEngine {
         this.p = p;
     }
 
-    public void openMarket () {
+    public void openMarket() {
         int option = 0;
         while (option != 5) {
-            ui.printMarketMenu(p.getInventory().getGold());
-            option = ui.getOption(5);
-            switch (option) {
-                case 1:
-                    HealingPotion potion = new HealingPotion("Healing Potion", 15, 30);
-                    if (validateSale(potion)) {
+            try {
+                ui.printMarketMenu(p.getInventory().getGold());
+                option = ui.getOption(5);
+                switch (option) {
+                    case 1:
+                        HealingPotion potion = new HealingPotion("Healing Potion", 15, 30);
                         p.addItemToInventory(potion);
                         p.spendGold(potion.getPrice());
-                    }
-                    break;
-                case 2: swordSale("wooden");
-                    break;
-                case 3: swordSale("stone");
-                    break;
-                case 4: swordSale("golden");
-                    break;
-                case 5: ui.printMessage("See you later...");
-                    break;
+                        break;
+                    case 2:
+                        swordSale("wooden");
+                        break;
+                    case 3:
+                        swordSale("stone");
+                        break;
+                    case 4:
+                        swordSale("golden");
+                        break;
+                    case 5:
+                        ui.printMessage("See you later...");
+                        break;
+                }
+            } catch (InsufficientGoldException | InventoryFullException | AlreadyHasItemException e) {
+                ui.printMessage(e.getMessage());
             }
         }
+
     }
 
     private Sword generateSword(String type) {
         return switch (type) {
-            case "stone" -> new Sword("Stone Sword",150, 10);
-            case "golden" -> new Sword("Golden Sword",250, 25);
-            default -> new Sword("Wooden Sword",15, 5);
+            case "stone" -> new Sword("Stone Sword", 150, 10);
+            case "golden" -> new Sword("Golden Sword", 250, 25);
+            default -> new Sword("Wooden Sword", 15, 5);
         };
     }
 
 
-    private void swordSale (String type) {
-       Sword sword = generateSword(type);
-       if (validateSale(sword)) {
-           if (!p.hasSword()) {
-               p.spendGold(sword.getPrice());
-               p.addItemToInventory(sword);
-           }
-       }
-    }
-
-    private boolean validateSale (Item item) {
-        return p.getGold() >= item.getPrice();
+    private void swordSale(String type) {
+        Sword sword = generateSword(type);
+        p.addItemToInventory(sword);
+        p.spendGold(sword.getPrice());
     }
 }
